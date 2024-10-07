9600119d84d0f9830bfe0278d7fd458d178e6014
caio2983 <caioluna2002@gmail.com>
2024-10-07 20:33:21 -0300

    feat : added skybox

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement); 
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x0077ff }); 
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+    
+
+
+function animate() {
+    requestAnimationFrame(animate); 
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera); 
+}
+
+animate(); 
+
+window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
+
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('resources/tundra_northern_lights.jpg');
+
+
+texture.mapping = THREE.EquirectangularReflectionMapping;
+scene.background = texture;
