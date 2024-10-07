64cf6135d73d41128cde11e8fdc6d65b72623bd2
caio2983 <caioluna2002@gmail.com>
2024-10-07 20:43:13 -0300

    feat : added floor

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/j
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 8;
+camera.position.y = 4;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); 
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x0077ff }); 
+const material = new THREE.MeshBasicMaterial({ color: '#242a54' }); 
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 2;
 scene.add(cube);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -46,3 +48,12 @@ const texture = textureLoader.load('resources/tundra_northern_lights.jpg');
 
 texture.mapping = THREE.EquirectangularReflectionMapping;
 scene.background = texture;
+
+
+const geometry_plane = new THREE.PlaneGeometry( 100, 100 );
+const material_plane = new THREE.MeshBasicMaterial( {color:'gray', side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry_plane, material_plane );
+plane.rotation.x = - Math.PI / 2;
+
+plane.position.x = 0;
+scene.add( plane );
